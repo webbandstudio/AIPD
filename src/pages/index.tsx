555a00b07ad170d0e92929e71469bb5e33c207a2
IvanSidorenkov <ivan.sidarenkov@weband.eu>
2023-05-18 13:43:05 +0300

    Create FAQ component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Delivery from "@/sections/delivery/Delivery";
+import FaqElement from "@/components/faqElement/FaqElement";
 
 const Home = () => {
   return (
     <main>
       <Delivery />
+      <FaqElement title="Как долго ждать доставку" text="Очень долго :)))))" />
     </main>
   );
 };
