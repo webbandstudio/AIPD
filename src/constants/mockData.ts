e455ab00988d9dde962da39b88107536b94704d6
IvanSidorenkov <ivan.sidarenkov@weband.eu>
2023-05-26 09:05:39 +0300

    Create goods section

--- src/constants/mockData.ts
+++ src/constants/mockData.ts
@@ -28,3 +28,86 @@ export const SHOPS = [
     image: ''
   },
 ];
+
+export const GOODS = [
+  {
+    id: 'Airpods Max',
+    title: 'Airpods Max',
+    economy: 240,
+  },
+  {
+    id: 'IKEA HEMNES',
+    title: 'IKEA HEMNES',
+    economy: 50,
+  },
+  {
+    id: 'Куртка Zara',
+    title: 'Куртка Zara',
+    economy: 50,
+  },
+  {
+    id: 'MacBook Pro 16 M2 Pro',
+    title: 'MacBook Pro 16 M2 Pro',
+    economy: 350,
+  },
+  {
+    id: 'Yeezy 500 Blush',
+    title: 'Yeezy 500 Blush',
+    economy: 120,
+  },
+  {
+    id: 'Zig Kinetica 2.5 Edge',
+    title: 'Zig Kinetica 2.5 Edge',
+    economy: 50,
+  },
+  {
+    id: 'New Balance U327WED',
+    title: 'New Balance U327WED',
+    economy: 80,
+  },
+  {
+    id: 'Air Jordan 1 Mid',
+    title: 'Air Jordan 1 Mid',
+    economy: 50,
+  },
+  {
+    id: 'Sony Playstation 5',
+    title: 'Sony Playstation 5',
+    economy: 240,
+  },
+  {
+    id: 'Adidas Forum Low',
+    title: 'Adidas Forum Low',
+    economy: 50,
+  },
+  {
+    id: 'Giorgio Armani Si',
+    title: 'Giorgio Armani Si',
+    economy: 240,
+  },
+  {
+    id: 'SAMSUNG WW90T634DLH',
+    title: 'SAMSUNG WW90T634DLH',
+    economy: 150,
+  },
+  {
+    id: 'LG OLED55C21LA',
+    title: 'LG OLED55C21LA',
+    economy: 320,
+  },
+  {
+    id: 'Nike AirJordan 1',
+    title: 'Nike AirJordan 1',
+    economy: 50,
+  },
+  {
+    id: 'BEATS Studio Buds',
+    title: 'BEATS Studio Buds',
+    economy: 56,
+  },
+  {
+    id: 'Michelin Primacy 4',
+    title: 'Michelin Primacy 4',
+    economy: 250,
+  },
+];
