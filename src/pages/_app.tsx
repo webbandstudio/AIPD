73a9e21b82cda94631acb29b86466feca7b09769
IvanSidorenkov <ivan.sidarenkov@weband.eu>
2023-06-09 16:58:02 +0300

    Add final GTM to the site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import TagManager from 'react-gtm-module';
 
 import '@styles/default.scss';
 import Layout from '@layouts/Layout';
 import GoogleAnalytics from '@analytics/googleAnalytics';
-import Gtm from '@analytics/gtm';
+import { GTM_ID } from '@constants/constants';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: GTM_ID });
+  }, []);
+
   return (
     <>
       <Head>
@@ -16,7 +21,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1.0"
         />
       </Head>
-      <Gtm />
       <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
